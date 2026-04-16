d7d5e0cb34d9e4a43a50a63111d32ca8703918fc
KristofferStrube <KristofferStrube@users.noreply.github.com>
2026-04-16 20:44:04 +0000

    deploy: 6f36bb91070fb3ba3ba0b803ee86cf84f6049296

---
+++ _content/KristofferStrube.Blazor.WebMCP/KristofferStrube.Blazor.WebMCP.js
@@ -0,0 +1,26 @@
+export function registerTool(tool, options) {
+    navigator.modelContext.registerTool({
+        "name": tool.name,
+        "description": tool.description,
+        "inputSchema": tool.inputSchema,
+        "execute": async (input, client) => {
+            if (client != undefined && client != null) {
+                client = DotNet.createJSObjectReference(client);
+            }
+            return await tool.objRef.invokeMethodAsync("InvokeExecuteAsync", input, client)
+        },
+        "annotations": tool.annotations == null ? undefined : tool.annotations,
+    }, options);
+}
+
+export async function requestUserInteraction(modelContextClient, callback) {
+    return await modelContextClient.requestUserInteraction(async () => await callback.invokeMethodAsync("InvokeCallback"))
+}
+
+export function hasModelContext() {
+    return navigator.modelContext != undefined;
+}
+
+export function hasUnregisterToolFunction() {
+    return navigator.modelContext.unregisterTool != undefined;
+}
